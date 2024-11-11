3df5ffd597faadd243bd0020cc70fe819356ac8a
jeff <jeffersonfloresherrera@gmail.com>
2024-11-11 13:13:52 -0500

    chore: add basic minimal api for customers

---
+++ src/DevsuCustomer.Api/Apis/CustomerApi.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace DevsuCustomer.Api.Apis;
+
+public static class CustomerApi
+{
+    public static class Routes
+    {
+        public const string GroupName = "api/clientes";
+    }
+
+    public static void MapCustomerApi(this IEndpointRouteBuilder app)
+    {
+        var api = app.MapGroup(Routes.GroupName);
+
+        api.MapGet("/", GetCustomer);
+    }
+
+    public static async Task<Ok> GetCustomer()
+    {
+        return TypedResults.Ok();
+    }
+}
