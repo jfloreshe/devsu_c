77e488d51378ba312bab78b61e5ebbd9b017b2f2
jeff <jeffersonfloresherrera@gmail.com>
2024-11-12 04:44:35 -0500

    chore: add migrations for account ms

--- entregables/BaseDatos.sql
+++ entregables/BaseDatos.sql
@@ -46,3 +46,80 @@ GO
 COMMIT;
 GO
 
+-----------------------------------------------MS Account
+IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+CREATE TABLE [__EFMigrationsHistory] (
+    [MigrationId] nvarchar(150) NOT NULL,
+    [ProductVersion] nvarchar(32) NOT NULL,
+    CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Accounts] (
+    [AccountId] bigint NOT NULL IDENTITY,
+    [AccountNumber] nvarchar(450) NOT NULL,
+    [AccountType] nvarchar(max) NOT NULL,
+    [OpeningBalance] decimal(18,2) NOT NULL,
+    [State] bit NOT NULL,
+    [CustomerId] uniqueidentifier NOT NULL,
+    CONSTRAINT [PK_Accounts] PRIMARY KEY ([AccountId])
+    );
+GO
+
+CREATE TABLE [Customers] (
+    [CustomerId] uniqueidentifier NOT NULL,
+    [Name] nvarchar(200) NOT NULL,
+    CONSTRAINT [PK_Customers] PRIMARY KEY ([CustomerId])
+    );
+GO
+
+CREATE TABLE [Transactions] (
+    [TransactionId] uniqueidentifier NOT NULL,
+    [DateCreation] datetime2 NOT NULL,
+    [Type] nvarchar(max) NOT NULL,
+    [TransactionValue] decimal(18,2) NOT NULL,
+    [Balance] decimal(18,2) NOT NULL,
+    [AccountId] bigint NOT NULL,
+    CONSTRAINT [PK_Transactions] PRIMARY KEY ([TransactionId]),
+    CONSTRAINT [FK_Transactions_Accounts_AccountId] FOREIGN KEY ([AccountId]) REFERENCES [Accounts] ([AccountId]) ON DELETE CASCADE
+    );
+GO
+
+CREATE TABLE [AccountTransactionLog] (
+    [Id] bigint NOT NULL IDENTITY,
+    [DateCreation] datetime2 NOT NULL,
+    [TransactionId] uniqueidentifier NOT NULL,
+    [PreviousTypeTransaction] nvarchar(max) NOT NULL,
+    [NewTypeTransaction] nvarchar(max) NOT NULL,
+    [PreviousTransactionValue] decimal(18,2) NOT NULL,
+    [NewTransactionValue] decimal(18,2) NOT NULL,
+    [PreviousBalance] decimal(18,2) NOT NULL,
+    [NewBalance] decimal(18,2) NOT NULL,
+    CONSTRAINT [PK_AccountTransactionLog] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_AccountTransactionLog_Transactions_TransactionId] FOREIGN KEY ([TransactionId]) REFERENCES [Transactions] ([TransactionId]) ON DELETE CASCADE
+    );
+GO
+
+CREATE INDEX [IX_AccountTransactionLog_TransactionId] ON [AccountTransactionLog] ([TransactionId]);
+GO
+
+CREATE UNIQUE INDEX [IX_Accounts_AccountNumber] ON [Accounts] ([AccountNumber]);
+GO
+
+CREATE INDEX [IX_Accounts_CustomerId] ON [Accounts] ([CustomerId]);
+GO
+
+CREATE INDEX [IX_Transactions_AccountId] ON [Transactions] ([AccountId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20241112094150_AddAccountMsSchema', N'8.0.10');
+GO
+
+COMMIT;
+GO
---
+++ src/DevsuAccount.Api/Infrastructure/Persistence/Migrations/20241112094150_AddAccountMsSchema.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevsuAccount.Api.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccountMsSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    AccountId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountNumber = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AccountType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OpeningBalance = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    State = table.Column<bool>(type: "bit", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.AccountId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    TransactionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateCreation = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TransactionValue = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Balance = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    AccountId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.TransactionId);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "AccountId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AccountTransactionLog",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateCreation = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TransactionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PreviousTypeTransaction = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NewTypeTransaction = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PreviousTransactionValue = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    NewTransactionValue = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PreviousBalance = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    NewBalance = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountTransactionLog", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AccountTransactionLog_Transactions_TransactionId",
+                        column: x => x.TransactionId,
+                        principalTable: "Transactions",
+                        principalColumn: "TransactionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountTransactionLog_TransactionId",
+                table: "AccountTransactionLog",
+                column: "TransactionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_AccountNumber",
+                table: "Accounts",
+                column: "AccountNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_CustomerId",
+                table: "Accounts",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_AccountId",
+                table: "Transactions",
+                column: "AccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccountTransactionLog");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
